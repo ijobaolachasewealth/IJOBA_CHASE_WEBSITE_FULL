76c3dbbfb437bc5208a020f338f7dc14d6eab1d8
ijobaolachasewealth <165973566+ijobaolachasewealth@users.noreply.github.com>
2025-07-10 00:26:10 +0100

    Update nav-toggle.js

--- nav-toggle.js
+++ nav-toggle.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("nav-links");
+
+  hamburger.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+    hamburger.classList.toggle("is-active");
+  });
+
+  document.querySelectorAll("#nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+      navLinks.classList.remove("active");
+      hamburger.classList.remove("is-active");
+    });
+  });
+});
 
